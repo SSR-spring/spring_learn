820fa0e267c276a9247e2e0c02718014e9acb63d
CHOIY3SUNG <choiyesung2848@naver.com>
2022-11-23 11:19:39 +0900

    commit

---
+++ src/main/java/com/example/ssrspring/SsrSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssrspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsrSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsrSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ssrspring/SsrSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssrspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsrSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
